96c6a400854587db408fcf125bda99bc93f5c349
Janish-1 <janish.pancholi11@gmail.com>
2024-04-26 19:02:12 +0530

    ready for production

--- CivilVendor/settings.py
+++ CivilVendor/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g*_q#m7+n!(@4c$=*j8jv&bz6^gffltw7uuu99p&9&ym_7wi-5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
